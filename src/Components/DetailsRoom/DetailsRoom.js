18d0e19c0dabac3d5f143bb3f27a9e13361bd4a2
zimlama <zimlama@gmail.com>
2023-03-03 10:43:02 -0500

    arreglos para subir a railway 10.42AM

--- src/Components/DetailsRoom/DetailsRoom.js
+++ src/Components/DetailsRoom/DetailsRoom.js
@@ -2,7 +2,10 @@
 // ﻿#13 52.26   Line 1:17:  'useEffect' is defined but never used          no-unused-vars
 // import React, { useEffect } from " react";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+// #13 63.85 src/Components/DetailsRoom/DetailsRoom.js
+// ﻿#13 63.85   Line 5:10:  'useDispatch' is defined but never used  no-unused-vars
+// import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import {
   Card,
@@ -19,7 +22,9 @@ import {
 import { useNavigate } from "react-router-dom";
 import { IoBedSharp } from "react-icons/io5";
 
-import allActions from "../../Redux/actions/";
+// #13 63.85 src/Components/DetailsRoom/DetailsRoom.js
+// ﻿#13 63.85   Line 22:8:  'allActions' is defined but never used   no-unused-vars
+// import allActions from "../../Redux/actions/";
 // #13 52.26 src/Components/DetailsRoom/DetailsRoom.js
 // ﻿#13 52.26   Line 20:9:  'takeDate' is assigned a value but never used  no-unused-vars
 // const { takeDate } = allActions;
